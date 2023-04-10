9a590331f1929d7a5bb0a58ef06d302c24913af0
tehi_kim <k.tehi21@gmail.com>
2023-04-10 22:16:41 +0000

    create some example data

---
+++ db/seeds/06_categories.sql
@@ -0,0 +1,7 @@
+
+-- Categories table seeds here
+INSERT INTO categories (category) VALUES ('TO_EAT');
+INSERT INTO categories (category) VALUES ('TO_WATCH');
+INSERT INTO categories (category) VALUES ('TO_BUY');
+INSERT INTO categories (category) VALUES ('TO_READ');
+
