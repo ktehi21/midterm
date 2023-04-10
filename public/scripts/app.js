85a410a7e4ac1dda8b5913ff2bf636b6b5213f41
B-Waterman <brooke.waterman@outlook.com>
2023-04-10 12:19:27 -0700

    Create js function to animate collapsible list 'button'. Re-name img file to clearer 'images'.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,18 @@
 // Client facing scripts here
-const coll = document.getElementsByClassName("collapsible");
+$(document).ready(function() {
+  const coll = document.getElementsByClassName("collapsible");
 
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    const content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-};
+  for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      const content = this.nextElementSibling;
+      if (content.style.display === "block") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "block";
+      }
+    });
+  };
+
+});
+
