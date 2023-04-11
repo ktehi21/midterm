85d46a9d975ffd9cf5801d3c53b8bc29ae0c0bf3
B-Waterman <brooke.waterman@outlook.com>
2023-04-10 19:51:27 -0700

    Create render, sendToList, loadTasks funcs, & complete new-task-form & createTaskElement func.

--- public/scripts/tasks.js
+++ public/scripts/tasks.js
@@ -19,11 +19,55 @@ $(document).ready(function () {
             <span class="checkable"> ${escape(task.content.text)}
           </label>
         </p>
-        <div class="creation-date">${timeago.format(task)}</div>
         </article>
     `);
+    return $task;
+  };
+
+  const sendToList = function(task) {
+    if (task.category === 'eat') {
+      $(".eat.todo-list")
+    }
+    if (task.category === 'watch') {
+      $(".watch.todo-list").append(task);
+    }
+    if (task.category === 'buy') {
+      $(".buy.todo-list").append(task);
+    }
+    if (task.category === 'read') {
+      $(".read.todo-list").append(task);
+    }
+  };
+
+  const renderToDo = function(tasks) {
+    const $container = $("#lists");
+    $container.empty();
+    tasks.forEach(task => {
+      const newTask = createTaskElement(task);
+      sendToList(newTask);
+    });
   };
 
   //Submit a new task to list from submit form!
-  $("#new-task-form").submit
+  $("#new-task-form").submit(function(event) {
+    event.preventDefault();
+    const newTask = $(this).serialize();
+    const input = $("#text").val();
+
+    $.post({
+      method: 'POST',
+      url: '/tasks',
+      data: newTask
+    }).then(loadToDo);
+
+  });
+
+  const loadToDo = function() {
+    $.ajax({
+      url: '/tasks',
+      method: 'GET'
+    }).then(renderToDo);
+  };
+
+  loadToDo();
 });
