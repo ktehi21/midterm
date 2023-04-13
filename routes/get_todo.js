543ba34678dda170c6e3983f0e787f69bed673e0
tehi_kim <k.tehi21@gmail.com>
2023-04-13 05:14:53 +0000

    change the get to post, req.param.word to req.body.task, and send the data as res.json({data})

--- routes/get_todo.js
+++ routes/get_todo.js
@@ -4,13 +4,13 @@ const router  = express.Router();
 const todoQueries = require('../db/queries/todo_data');
 const {categoryDecision} = require('./category-decision');
 
-router.get('/:word', (req, res) => {
-  const word = req.params.word;
-  console.log("get todo", word);
+router.post('/', (req, res) => {
+  const word = req.body.task;
+  // console.log("get todo", word);
   categoryDecision(word)
   .then(data => {
-    console.log(data);
-    res.json(data)
+    // console.log(data);
+    res.json({data})
   })
   .catch(err => console.log("getTodo, error", err.message));
 });
