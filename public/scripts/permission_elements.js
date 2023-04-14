a7f968a9e1782ad8c54f3380f95884198a44e68a
B-Waterman <brooke.waterman@outlook.com>
2023-04-14 11:46:33 -0700

    Refactor to utilise JQuery.

--- public/scripts/permission_elements.js
+++ public/scripts/permission_elements.js
@@ -1,24 +1,13 @@
-// function showElement() {
-//   // Get references to the elements with the given class names
-//   const wrapper = document.querySelector('.wrapper');
-//   const initialPageLoad = document.querySelector('.initial-page-load');
-
-//   // Show the wrapper element and hide the initial-page-load element
-//   wrapper.style.display = 'block';
-//   initialPageLoad.style.display = 'none';
-
-//   // Update the button text
-//   const button = document.querySelector('button');
-//   button.innerHTML = 'Hello, Andrea!';
-// };
-
 function showElement() {
+  // Get references to the elements with the given class names
   const wrapper = $('.wrapper');
   const initialPageLoad = $('.initial-page-load');
 
+  // Show the wrapper element and hide the initial-page-load element
   wrapper.css('display', 'block');
   initialPageLoad.css('display', 'none');
 
+  // Update the button text
   const button = $('.login');
   button.html('Hello, Andrea!');
 };
